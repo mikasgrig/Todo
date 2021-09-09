cd64909503a6e76244645225ac1078a70694b4e8
Mikas <mikasgrig@gmail.com>
2021-09-09 14:52:45 +0300

    todoseClient--Done

--- todosClient.js
+++ todosClient.js
@@ -1,27 +1,56 @@
+async function getTodos(){
+  let response = await fetch('https://localhost:5001/todos');
+  var todos =  await response.json();
 
-// async function getPost(id){
-//     let response = await fetch('https://jsonplaceholder.typicode.com/posts/' + id);
-// var body =  await response.json();
-
-//     console.log(body)
-// }
-
-// async function cratePost(){
-//    let response= await fetch('https://jsonplaceholder.typicode.com/posts', {
-//   method: 'POST',
-//   body: JSON.stringify({
-//     title: 'Post request from JS',
-//     body: 'Post reqest',
-//     userId: 1,
-//   }),
-//   headers: {
-//     'Content-type': 'application/json; charset=UTF-8',
-//   },
-// })
-// let body = await response.json();
-// console.log(body)
-// }
-
-//cratePost();
-//getTodos();
-//crateTodo();
+  return todos;
+} 
+async function crateTodo(title, description, difficulty){
+  let response= await fetch('https://localhost:5001/todos', {
+ method: 'POST',
+ body: JSON.stringify({
+      title: `${title}`,
+      description: `${description}`,
+      difficulty: `${difficulty}`
+ }),
+ headers: {
+   'Content-type': 'application/json; charset=UTF-8',
+ },
+})
+let body = await response.json();
+console.log(body)
+return body
+}
+async function deleteTodo(id){
+ fetch('https://localhost:5001/todos' + id, {
+method: 'DELETE',
+});
+};
+async function uptadeTodo(id, title, description, difficulty){
+  let response = await fetch('https://localhost:5001/todos', {
+    method: 'PUT',
+    body: JSON.stringify({
+      id:`${id}`,
+      title: `${title}`,
+      description: `${description}`,
+      difficulty: `${difficulty}`,
+    }),
+    headers: {
+      'Content-type': 'application/json; charset=UTF-8',
+    }
+  })
+  let body = await response.json();
+  return body;
+};
+async function updateIsDoneTodo(id, isDone){
+  let response = await fetch('https://localhost:5001/todos' + id, {
+    method: 'PUT',
+    body: JSON.stringify({
+      isDone: `${isDone}`,
+    }),
+    headers: {
+      'Content-type': 'application/json; charset=UTF-8',
+    }
+  })
+  let body = await response.json();
+  return body;
+};
